c7b894bcdf99fc3f462ac9449953369860654549
Paulo Salvatore <salvatoregames@gmail.com>
2018-04-03 10:30:15 -0300

    Início do desenvolvimento do sistema de Waypoints

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour {
+public class Waypoint : MonoBehaviour
+{
+    private Waypoint[] waypoints;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        waypoints = FindObjectsOfType<Waypoint>();
+    }
 }
